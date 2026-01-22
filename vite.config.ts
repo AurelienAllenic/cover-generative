5dcfa3bce1f060864fb330e48569524efa102072
AurelienAllenic <aurelien.allenic@gmail.com>
2026-01-22 20:13:21 +0100

    (feat) added more config for reactt on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-  plugins: [glsl()],
+  plugins: [react(), glsl()],
   optimizeDeps: {
     exclude: ['path', 'os', 'process', 'fs'], // Ignore ces modules lors de l'optimisation
   },
